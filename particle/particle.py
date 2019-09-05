36bc844c045b727b06644a8a480438512b06681d
Isabella Martínez <44677917+BelisaDi@users.noreply.github.com>
2019-09-05 12:53:24 -0500

    Euler's Method

--- particle/particle.py
+++ particle/particle.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+G = 1
+
 class Particle:
-    
+
     def __init__(self, label, x0, y0, v0, alpha0, m0 = 1, t0 = 0):
         self.lb = label
         self.x, self.y = x0, y0
@@ -18,6 +20,18 @@ class Particle:
         string += "position = ({:.4f}, {:.4f})\n".format(self.x, self.y)
         string += "velocity = ({:.4f}, {:.4f})\n".format(self.vx, self.vy)
         return string
+
+    def step(self, dt):
+        self.x = self.x + dt*self.vx
+        self.y = self.y + dt*self.vy
+        self.vx = self.vx
+        self.vy = self.vy -(dt*G)
+        self.t = self.t + dt
+
+    def get_state(self):
+        return self.x, self.y, self.vx, self.vy, self.t
+
+
 if __name__ == "__main__":
   mars = Particle("Mars",1,2,100,60)
   print(mars)
