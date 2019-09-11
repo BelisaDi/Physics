a2fe436079868b03b7f7c173e906c5897227f3c2
Isabella Martínez <44677917+BelisaDi@users.noreply.github.com>
2019-09-10 20:37:07 -0500

    Add files via upload

--- particle/particle.py
+++ particle/particle.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 
-G = 1
+G = 1.
+DRAG = 4.
 
 class Particle:
 
@@ -11,8 +12,8 @@ class Particle:
         self.x, self.y = x0, y0
         self.vx = v0 * np.cos(np.radians(alpha0))
         self.vy = v0 * np.sin(np.radians(alpha0))
-        self.m = m0
-        self.t = t0
+        self.m, self.t = m0, t0
+        self.force = None
 
     def __str__(self):
         string = self.lb + "\n"
@@ -21,46 +22,36 @@ class Particle:
         string += "velocity = ({:.4f}, {:.4f})\n".format(self.vx, self.vy)
         return string
 
-    def euler_step(self, dt, c):
-        #This is with F = -mg
-        # self.x = self.x + dt*self.vx
-        # self.y = self.y + dt*self.vy
-        # self.vx = self.vx
-        # self.vy = self.vy -(dt*G)
-        # self.t = self.t + dt
+    def euler_step(self, dt):
+        state = self.x, self.y, self.vx, self.vy, self.t
+        dxdt, dydt, dvxdt, dvydt, dtdt = self.force.get_force(state)
+        self.x = self.x + dxdt * dt
+        self.y = self.y + dydt * dt
+        self.vx = self.vx + dvxdt * dt
+        self.vy = self.vy + dvydt * dt
+        self.t = self.t + dtdt * dt
 
-        #This is with F = mg + cV (drag resistance)
-        self.x = self.x + dt*self.vx
-        self.y = self.y + dt*self.vy
-        self.vx = self.vx*(1 - (c/self.m)*dt)
-        if self.vy >= 0:
-            self.vy = self.vy*(1 - (dt*c)/self.m) - dt*G
-        else:
-            self.vy = self.vy*(1 + (dt*c)/self.m) - dt*G
-        self.t = self.t + dt
+    def euler_cromer_step(self, dt):
+        state = self.x, self.y, self.vx, self.vy, self.t
+        dxdt, dydt, dvxdt, dvydt, dtdt = self.force.get_force(state)
+        self.vx = self.vx + dvxdt * dt
+        self.vy = self.vy + dvydt * dt
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
+        self.t = self.t + dtdt * dt
 
-    def euler_cromer_step(self, dt, c):
-        #This is with F = -mg
-        # self.vx = self.vx
-        # self.vy = self.vy -(dt*G)
-        # self.x = self.x + dt*self.vx
-        # self.y = self.y + dt*self.vy
-        # self.t = self.t + dt
-
-        #This is with F = mg + cV (drag resistance)
-        self.vx = self.vx*(1 - (c/self.m)*dt)
-        if self.vy >= 0:
-            self.vy = self.vy*(1 - (dt*c)/self.m) - dt*G
-        else:
-            self.vy = self.vy*(1 + (dt*c)/self.m) - dt*G
-        self.x = self.x + dt*self.vx
-        self.y = self.y + dt*self.vy
-        self.t = self.t + dt
+    def midpoint_step(self, dt):
+        state = self.x, self.y, self.vx, self.vy, self.t
+        dxdt, dydt, dvxdt, dvydt, dtdt = self.force.get_force(state)
+        vx_old, vy_old = self.vx, self.vy
+        self.vx = self.vx + dvxdt * dt
+        self.vy = self.vy + dvydt * dt
+        self.x = self.x + (0.5) * (vx_old + self.vx) * dt
+        self.y = self.y + (0.5) * (vy_old + self.vy) * dt
+        self.t = self.t + dtdt * dt
 
     def get_state(self):
         return self.x, self.y, self.vx, self.vy, self.t
 
-
-if __name__ == "__main__":
-  mars = Particle("Mars",1,2,100,60)
-  print(mars)
+    def set_force(self, netforce):
+        self.force = netforce
