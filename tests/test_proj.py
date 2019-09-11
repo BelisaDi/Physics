10d0904a854d76e7e8052f1be2d38f03bfe4bb34
Isabella Martínez <44677917+BelisaDi@users.noreply.github.com>
2019-09-10 20:37:24 -0500

    Add files via upload

--- tests/test_proj.py
+++ tests/test_proj.py
@@ -1,93 +1,89 @@
 import sys
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
-
+import matplotlib.pyplot as plt
 sys.path.insert(0, "../")
+
 import particle.particle as pt
+import forces.forces as fr
 
 #Function Definitions
 
-def exact_sol(x0, y0, v0, alpha0, t):
-    v0x = v0 * np.cos(np.radians(alpha0))
-    v0y = v0 * np.sin(np.radians(alpha0))
-    x = x0 + v0x*t
-    y = y0 + v0y*t - (0.5*pt.G)*t**2
-    return x,y
+def resistive_falling(state, params):
+    xp, yp, vxp, vyp, _ = state
+    mass, grav, drag = params
+    axp = -drag * vxp / mass
+    ayp = -grav - drag * vyp / mass
+    return vxp, vyp, axp, ayp, 1.
 
-def euler(particle, xlist, ylist, tlist):
-    while particle.y >= 0:
-        particle.euler_step(0.01, 4)
-        x,y,vx,vy,t = particle.get_state()
-        if particle.y < 0: break
-        xlist.append(x)
-        ylist.append(y)
-        tlist.append(t)
+def euler(ball, xpos, ypos, tpos):
+    while True:
+        xc, yc, _, _, tc = ball.get_state()
+        if yc < 0: break
+        xpos.append(xc)
+        ypos.append(yc)
+        tpos.append(tc)
+        ball.euler_step(deltat)
 
-def euler_cromer(particle, xlist, ylist, tlist):
-    while particle.y >= 0:
-        particle.euler_cromer_step(0.01, 4)
-        x,y,vx,vy,t = particle.get_state()
-        if particle.y < 0: break
-        xlist.append(x)
-        ylist.append(y)
-        tlist.append(t)
+def euler_cromer(ball, xpos, ypos, tpos):
+    while True:
+        xc, yc, _, _, tc = ball.get_state()
+        if yc < 0: break
+        xpos.append(xc)
+        ypos.append(yc)
+        tpos.append(tc)
+        ball.euler_cromer_step(deltat)
+
+def midpoint(ball, xpos, ypos, tpos):
+    while True:
+        xc, yc, _, _, tc = ball.get_state()
+        if yc < 0: break
+        xpos.append(xc)
+        ypos.append(yc)
+        tpos.append(tc)
+        ball.midpoint_step(deltat)
 
 #Initial Variables and Lists
 
-x0, y0, v0, alpha0 = 0, 0.5, 1, 45
-pluto = pt.Particle("Pluto",x0, y0, v0, alpha0)
-pluto2 = pt.Particle("Pluto 2",x0, y0, v0, alpha0)
-x,y,vx,vy,t = pluto.get_state()
+m, x0, y0, v0, a0 = 1., 0., 0.5, 1., 45
+deltat = 0.01
+sim_params = m, pt.G, pt.DRAG
+
+ball = pt.Particle("Pelota 1", x0, y0, v0, a0, m)
+ball_force = fr.Forces(resistive_falling, sim_params)
+ball.set_force(ball_force)
+
+ball2 = pt.Particle("Pelota 2", x0, y0, v0, a0, m)
+ball2.set_force(ball_force)
+
+ball3 = pt.Particle("Pelota 3", x0, y0, v0, a0, m)
+ball3.set_force(ball_force)
 
 xposEuler = []
 yposEuler = []
-tpos = []
+tposEuler = []
 
 xposEulerCromer = []
 yposEulerCromer = []
 tposEulerCromer = []
 
-xposNum = []
-yposNum = []
+xposMidpoint = []
+yposMidpoint = []
+tposMidpoint = []
 
-xposEuler.append(x)
-yposEuler.append(y)
-tpos.append(t)
 
-xposEulerCromer.append(x)
-yposEulerCromer.append(y)
-tposEulerCromer.append(t)
+euler(ball, xposEuler, yposEuler, tposEuler)
+euler_cromer(ball2, xposEulerCromer, yposEulerCromer, tposEulerCromer)
+midpoint(ball3, xposMidpoint, yposMidpoint, tposMidpoint)
 
-x_error = []
-y_error = []
-
-#Execution of Numerical Methods and Exact Solutions
-euler(pluto, xposEuler, yposEuler, tpos)
-euler_cromer(pluto2, xposEulerCromer, yposEulerCromer, tposEulerCromer)
-
-for t in tpos:
-    xnum, ynum = exact_sol(x0, y0, v0, alpha0, t)
-    xposNum.append(xnum)
-    yposNum.append(ynum)
-
-#Error calculations
-for i in range(0, len(yposNum)):
-    err = abs(yposNum[i] - yposEuler[i])
-    y_error.append(err)
-
-for i in range(0, len(xposNum)):
-    err = abs(xposNum[i] - xposEuler[i])
-    x_error.append(err)
-
-#Plotting the results
+#Generate Plots
 fig, ax = plt.subplots()
-ax.plot(xposEulerCromer, yposEulerCromer, '--', label = 'Euler Cromer Solution')
-ax.plot(xposEuler, yposEuler, '-', label = 'Euler Solution')
-#ax.plot(tpos, y_error, '-', label = "Error Y")
-#ax.plot(tpos, x_error, '--', label = "Error X")
+ax.plot(xposEuler, yposEuler, '-', label='Euler')
+ax.plot(xposEulerCromer, yposEulerCromer, '--', label='Euler-Cromer')
+ax.plot(xposMidpoint, yposMidpoint, '--', label='Midpoint')
 
-ax.set(xlabel = "X Position", ylabel = "Y Position", title = "Projectile Motion: Euler's Methods")
+ax.set(xlabel='x (a.u.)', ylabel='y (a.u.)',
+       title='Projectile motion with drag')
 ax.grid()
 
+plt.legend()
 plt.show()
