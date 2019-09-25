f2fb1b922236a081fc480e9784b8f7ca4a84bc3d
Isabella Martínez <44677917+BelisaDi@users.noreply.github.com>
2019-09-25 06:18:00 -0500

    Now the numerical methods are in solver

--- particle/particle.py
+++ particle/particle.py
@@ -1,60 +1,33 @@
-# coding: utf-8
-
 import numpy as np
 
-G = 1.
-M = 1.
+GM = 4*(np.pi**2)
 DRAG = 4.
 
 class Particle:
 
-    def __init__(self, label, x0, y0, v0, alpha0, m0 = 1, t0 = 0):
+    def __init__(self, label, x0, y0, v0, alpha0, m0 = 1., t0 = 0.):
         self.lb = label
+        self.m, self.t = m0, t0
         self.x, self.y = x0, y0
-        self.v0, self.a0 = v0, alpha0
         self.vx = v0 * np.cos(np.radians(alpha0))
         self.vy = v0 * np.sin(np.radians(alpha0))
-        self.m, self.t = m0, t0
-        self.r = np.sqrt(self.x**2 + self.y**2)
         self.force = None
 
     def __str__(self):
-        string = self.lb + "\n"
-        string += "mass = {}, time = {}\n".format(self.m, self.t)
-        string += "position = ({:.4f}, {:.4f})\n".format(self.x, self.y)
-        string += "velocity = ({:.4f}, {:.4f})\n".format(self.vx, self.vy)
-        return string
-
-    def euler_step(self, dt):
-        state = self.x, self.y, self.vx, self.vy, self.r, self.t
-        dxdt, dydt, dvxdt, dvydt, dtdt = self.force.get_force(state)
-        self.x = self.x + dxdt * dt
-        self.y = self.y + dydt * dt
-        self.vx = self.vx + dvxdt * dt
-        self.vy = self.vy + dvydt * dt
-        self.t = self.t + dtdt * dt
-
-    def euler_cromer_step(self, dt):
-        state = self.x, self.y, self.vx, self.vy, self.r, self.t
-        dxdt, dydt, dvxdt, dvydt, dtdt = self.force.get_force(state)
-        self.vx = self.vx + dvxdt * dt
-        self.vy = self.vy + dvydt * dt
-        self.x = self.x + self.vx * dt
-        self.y = self.y + self.vy * dt
-        self.t = self.t + dtdt * dt
-
-    def midpoint_step(self, dt):
-        state = self.x, self.y, self.vx, self.vy, self.r, self.t
-        dxdt, dydt, dvxdt, dvydt, dtdt = self.force.get_force(state)
-        vx_old, vy_old = self.vx, self.vy
-        self.vx = self.vx + dvxdt * dt
-        self.vy = self.vy + dvydt * dt
-        self.x = self.x + (0.5) * (vx_old + self.vx) * dt
-        self.y = self.y + (0.5) * (vy_old + self.vy) * dt
-        self.t = self.t + dtdt * dt
+        strng = self.lb + "\n"
+        strng += "mass = {}, time = {}\n".format(self.m, self.t)
+        strng += "position = ({:.4f}, {:.4f})\n".format(self.x, self.y)
+        strng += "velocity = ({:.4f}, {:.4f})\n".format(self.vx, self.vy)
+        return strng
 
     def get_state(self):
-        return self.x, self.y, self.vx, self.vy, self.r, self.t
+        return self.x, self.y, self.vx, self.vy, self.t
+
+    def set_state(self, x, y, vx, vy, t):
+        self.x, self.y, self.vx, self.vy, self.t = x, y, vx, vy, t
 
     def set_force(self, netforce):
         self.force = netforce
+
+    def get_energy(self):
+        return .5*self.m*(self.vx**2 + self.vy**2) - (GM*self.m)/np.sqrt(self.x**2 + self.y**2)
